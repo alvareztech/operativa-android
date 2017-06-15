533e0eb2a52b95bff47b2fca6e97dba1be6cf5fb
Daniel Alvarez <a3dany@gmail.com>
2017-06-15 08:29:58 -0400

    Clean and prepare Preferences

--- OperationsResearch/src/main/java/com/danyalvarez/operationsresearch/GeneralPreferenceFragment.java
+++ OperationsResearch/src/main/java/com/danyalvarez/operationsresearch/GeneralPreferenceFragment.java
@@ -1,21 +1,12 @@
 package com.danyalvarez.operationsresearch;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.preference.PreferenceFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
-import com.danyalvarez.operationsresearch.adapters.lists.OptionListAdapter;
 
-/**
- * Created by daniel on 20/02/14.
- */
-
-public class ConfigurationFragment extends ListFragment {
+public class GeneralPreferenceFragment extends PreferenceFragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,13 +23,6 @@ public class ConfigurationFragment extends ListFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-    }
-
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-
 
     }
 }
