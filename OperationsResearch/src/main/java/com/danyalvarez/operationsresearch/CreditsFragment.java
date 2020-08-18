9d6bca7fca7274749092025592b23d980c2d9dba
Daniel Alvarez <daniel@alvarez.tech>
2020-08-17 23:23:20 -0400

    Update credits

--- OperationsResearch/src/main/java/com/danyalvarez/operationsresearch/CreditsFragment.java
+++ OperationsResearch/src/main/java/com/danyalvarez/operationsresearch/CreditsFragment.java
@@ -7,60 +7,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.DialogFragment;
 
 public class CreditsFragment extends DialogFragment {
 
-    private Button mEnviarCorreoButton;
-    private Button mIrWebButton;
-    private Button mYoutubeButton;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_credits, container, false);
 
-        mEnviarCorreoButton = rootView.findViewById(R.id.enviarCorreoButton);
-        mIrWebButton = rootView.findViewById(R.id.irWebButton);
-        mYoutubeButton = rootView.findViewById(R.id.youtubeButton);
+        Button webButton = rootView.findViewById(R.id.webButton);
+        webButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWeb("https://alvarez.tech/projects/operativa/");
+            }
+        });
+        TextView version = rootView.findViewById(R.id.versionTextView);
+        version.setText(getString(R.string.version, BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE));
 
         return rootView;
     }
 
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        mEnviarCorreoButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentCorreo = new Intent(Intent.ACTION_SEND);
-                intentCorreo.setType("text/plain");
-                intentCorreo.putExtra(Intent.EXTRA_EMAIL, new String[]{"daniel@alvarez.tech"});
-                intentCorreo.putExtra(Intent.EXTRA_SUBJECT, R.string.app_name);
-                intentCorreo.putExtra(Intent.EXTRA_TEXT, "");
-                startActivity(intentCorreo);
-
-            }
-        });
-
-        mIrWebButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri paginaWeb = Uri.parse("https://alvarez.tech");
-                Intent intentPaginaWeb = new Intent(Intent.ACTION_VIEW, paginaWeb);
-                startActivity(intentPaginaWeb);
-            }
-        });
-
-        mYoutubeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri uri = Uri.parse("https://www.youtube.com/channel/UCQBP46vtgU0OA91xL2tl-nQ");
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            }
-        });
-
+    private void openWeb(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
     }
 }
